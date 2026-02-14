6dacbc2671303a17acc6eec88801172de3e0e19e
IlhamArifinnn <arifinn2208@gmail.com>
2026-02-14 09:45:25 +0700

    Latihan: Menambahkan Provider dalam Tourism App

--- tourism_app/lib/screen/bookmark/bookmark_screen.dart
+++ tourism_app/lib/screen/bookmark/bookmark_screen.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:tourism_app/model/tourism.dart';
+import 'package:provider/provider.dart';
+import 'package:tourism_app/provider/detail/bookmark_list_provider.dart';
 import 'package:tourism_app/screen/home/tourism_card_widget.dart';
 import 'package:tourism_app/static/navigation_route.dart';
 
 class BookmarkScreen extends StatelessWidget {
-  const BookmarkScreen({Key? key}) : super(key: key);
+  const BookmarkScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Bookmark List")),
-      body: ListView.builder(
-        itemCount: bookmarkTourismList.length,
-        itemBuilder: (context, index) {
-          final tourism = bookmarkTourismList[index];
-          return TourismCard(
-            tourism: tourism,
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                NavigationRoute.detailRoute.name,
-                arguments: tourism,
-              );
-            },
-          );
+      body: Consumer<BookmarkListProvider>(
+        builder: (context, value, child) {
+          final bookmarkList = value.bookmarkList;
+          return switch (bookmarkList.isNotEmpty) {
+            true => ListView.builder(
+              itemCount: bookmarkList.length,
+              itemBuilder: (context, index) {
+                final tourism = bookmarkList[index];
+
+                return TourismCard(
+                  tourism: tourism,
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      NavigationRoute.detailRoute.name,
+                      arguments: tourism,
+                    );
+                  },
+                );
+              },
+            ),
+            _ => const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("No Bookmarked")],
+              ),
+            ),
+          };
         },
       ),
     );
